5df1364c4647af23e0678a6f3f7f7c9ebe24c5f6
Tobiasz Kędzierski <tobiasz.kedzierski@polidea.com>
2020-12-25 18:57:07 +0100

    Add favicon url

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,10 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import include, path
 from django.views import defaults as default_views
-from django.views.generic import TemplateView
+from django.views.generic import RedirectView, TemplateView
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
@@ -20,6 +21,12 @@ urlpatterns = [
     ),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
+    path(
+        "favicon.ico",
+        RedirectView.as_view(
+            url=staticfiles_storage.url("images/favicons/favicon.ico")
+        ),
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # API URLS

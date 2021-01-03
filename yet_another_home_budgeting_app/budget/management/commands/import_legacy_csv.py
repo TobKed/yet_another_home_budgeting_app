55ae22ad6e763f6de8d9c44c80c6ecd15ff03c8a
Tobiasz Kędzierski <tobiasz.kedzierski@polidea.com>
2021-01-03 14:04:28 +0100

    Add import_legacy_csv command

--- yet_another_home_budgeting_app/budget/management/commands/import_legacy_csv.py
+++ yet_another_home_budgeting_app/budget/management/commands/import_legacy_csv.py
@@ -30,11 +30,14 @@ class Command(BaseCommand):
             raise CommandError('User with email "%s" does not exist' % user_email)
 
         expenditure_count_start = Expenditure.objects.filter(user=user).count()
+        category_count_start = Category.objects.filter(user=user).count()
 
         self.stdout.write(
             self.style.SUCCESS(
                 'Successfully found user: "%s"\n'
-                "User Expenditure count: %d" % (user_email, expenditure_count_start)
+                "User Categories count: %d\n"
+                "User Expenditure count: %d"
+                % (user_email, category_count_start, expenditure_count_start)
             )
         )
 
@@ -45,18 +48,20 @@ class Command(BaseCommand):
         self._categories_registry = {c: None for c in categories}
         self._populate_categories(categories, user)
 
-        # for poll_id in options['poll_id']:
-        #     try:
-        #         poll = Poll.objects.get(pk=poll_id)
-        #     except Poll.DoesNotExist:
-        #         raise CommandError('Poll "%s" does not exist' % poll_id)
-        #
-        #     poll.opened = False
-        #     poll.save()
-
         self.stdout.write(
             self.style.SUCCESS(
                 '_categories_registry  "%s"' % str(self._categories_registry)
+            )
+        )
+
+        expenditure_count_end = Expenditure.objects.filter(user=user).count()
+        category_count_end = Category.objects.filter(user=user).count()
+
+        self.stdout.write(
+            self.style.SUCCESS(
+                "User Categories count: %d\n"
+                "User Expenditure count: %d"
+                % (category_count_end, expenditure_count_end)
             )
         )
 
@@ -83,61 +88,45 @@ class Command(BaseCommand):
                 continue
 
             category_path_list = category_path.split("-")
-            category_name = category_path_list[-1].capitalize()
-            category_path_list_length = len(category_path_list)
-            if len(category_path_list) == 1:
-                category_in_db = Category.objects.get_or_create(
-                    user=user, name=category_name, parent_id=parent_id
-                )[0]
-                self._categories_registry.update({category_path: category_in_db.id})
-            else:
-                for i in range(category_path_list_length):
-                    parent_category_path_list = category_path_list[slice(0, i)]
-                    parent_category_path = "-".join(parent_category_path_list)
-                    children_category_last_name = category_path_list[slice(i, i + 1)][0]
+            for i in range(len(category_path_list)):
+                parent_category_path_list = category_path_list[slice(0, i)]
+                parent_category_path = "-".join(parent_category_path_list)
+                children_category_last_name = category_path_list[slice(i, i + 1)][0]
 
-                    children_category_path = (
-                        "-".join([parent_category_path, children_category_last_name])
-                        if parent_category_path_list
-                        else children_category_last_name
+                children_category_path = (
+                    "-".join([parent_category_path, children_category_last_name])
+                    if parent_category_path_list
+                    else children_category_last_name
+                )
+
+                if parent_category_path and not self._categories_registry.get(
+                    parent_category_path
+                ):
+                    category_in_db = Category.objects.get_or_create(
+                        user=user,
+                        name=parent_category_path[-1].capitalize(),
+                        parent_id=self._categories_registry.get(
+                            "-".join(parent_category_path[0:-1])
+                        ),
+                    )[0]
+                    self._categories_registry.update(
+                        {parent_category_path: category_in_db.id}
                     )
 
-                    if parent_category_path and not self._categories_registry.get(
-                        parent_category_path
-                    ):
-                        category_in_db = Category.objects.get_or_create(
-                            user=user,
-                            name=parent_category_path[-1].capitalize(),
-                            parent_id=self._categories_registry.get(
-                                "-".join(parent_category_path[0:-1])
-                            ),
-                        )[0]
-                        self._categories_registry.update(
-                            {parent_category_path: category_in_db.id}
-                        )
-                        self.stdout.write(
-                            self.style.INFO(
-                                'Created category:  "%s"\n'
-                                "\tpath: %s"
-                                % (str(category_in_db), parent_category_path)
-                            )
-                        )
+                p_id = self._categories_registry.get(parent_category_path)
 
-                    p_id = self._categories_registry.get(parent_category_path)
-
-                    if not self._categories_registry.get(children_category_path):
-                        category_in_db = Category.objects.get_or_create(
-                            user=user,
-                            name=children_category_last_name.capitalize(),
-                            parent_id=p_id,
-                        )[0]
-                        self._categories_registry.update(
-                            {children_category_path: category_in_db.id}
+                if not self._categories_registry.get(children_category_path):
+                    category_in_db = Category.objects.get_or_create(
+                        user=user,
+                        name=children_category_last_name.capitalize(),
+                        parent_id=p_id,
+                    )[0]
+                    self._categories_registry.update(
+                        {children_category_path: category_in_db.id}
+                    )
+                    self.stdout.write(
+                        self.style.SUCCESS(
+                            'Created category:  "%s"\n'
+                            "\tpath: %s" % (str(category_in_db), children_category_path)
                         )
-                        self.stdout.write(
-                            self.style.INFO(
-                                'Created category:  "%s"\n'
-                                "\tpath: %s"
-                                % (str(category_in_db), children_category_path)
-                            )
-                        )
+                    )
